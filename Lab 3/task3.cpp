045928b699007dcf10d8397a07a4c29b02dc0aff
Muhammad Aamir <Muhammad Aamir>
2017-10-28 11:56:43 -0700

    Add project files.

---
+++ Lab 3/task3.cpp
@@ -0,0 +1,14 @@
+//Task 3
+#include<iostream>
+using namespace std;
+void main() {
+	int total, litetratemen, literatewomen, population = 80000;
+
+	litetratemen = population * 0.35;
+	literatewomen = population * 0.13;
+	cout << "total population in town :80,000" << endl;
+	cout << "population of literate men :" << litetratemen << endl;
+	cout << "population of literate women :" << literatewomen << endl;
+	system("pause");
+
+}
