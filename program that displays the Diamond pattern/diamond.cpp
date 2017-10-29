07722af7d46561ae5017a590fa5a94596fc3e111
aamirorbit <33173752+aamirorbit@users.noreply.github.com>
2017-10-29 02:40:21 -0700

    Add files via upload

---
+++ program that displays the Diamond pattern/diamond.cpp
@@ -0,0 +1,14 @@
+//program that displays the Diamond pattern.
+#include<iostream>
+using namespace std;
+void main()
+{
+	cout << "   .   " << endl;
+	cout << "  ...  " << endl;
+	cout << " ..... " << endl;
+	cout << "......." << endl;
+	cout << " ..... " << endl;
+	cout << "  ...  " << endl;
+	cout << "   .   " << endl;
+	system("pause");
+}
