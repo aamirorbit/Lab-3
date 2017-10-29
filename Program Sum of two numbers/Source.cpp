b2ffa556b31d10d723154ffa6930b79412a6d493
aamirorbit <33173752+aamirorbit@users.noreply.github.com>
2017-10-29 03:04:59 -0700

    AamirOrbit

---
+++ Program Sum of two numbers/Source.cpp
@@ -0,0 +1,18 @@
+//program sum of two num,
+#include <iostream>
+using namespace std;
+void main()
+{
+	double first, second, sum;
+
+	cout << "Enter two integer:";
+	cin >> first >> second;
+
+
+	sum = first + second;
+
+	cout << first << "+" << second << "=" << sum;
+	
+	system("pause");
+	
+}
